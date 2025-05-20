8ff72c8ac90dfdb34f6591c2d98d71df0a37ba4a
nora2605 <nora.ja2605@gmail.com>
2025-05-20 19:02:21 +0200

    cool stuff

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+using System.IO;
+using System.Reflection;
+
+Raylib.InitWindow(800, 480, "Job At OEIS");
+Raylib.InitAudioDevice();
+
+var music = Raylib.LoadMusicStream("Assets/Marquez.qoa");
+Raylib.PlayMusicStream(music);
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.UpdateMusicStream(music);
+    Raylib.ClearBackground(Color.White);
+    var t = Raylib.MeasureText("Hello, world!", 20);
+    Raylib.DrawText("Hello, World!", 400 - t / 2, 230, 20, Color.Black);
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseAudioDevice();
+Raylib.CloseWindow();
